b3277ba747502be98338532cbb60b3a41b72d095
Z <wzhang@stu.xjtu.edu.cn>
2024-11-25 21:26:02 +0800

    mask feature

--- scripts/mask.js
+++ scripts/mask.js
@@ -1,5 +1,5 @@
 // 检测当前时间是否为工作时间
-function isWorkingHours(begin = 8, end = 21) {
+function isWorkingHours(begin = 8, end = 23) { // 开发阶段，暂时先设定为8点到23点
     const now = new Date();
     const hour = now.getHours();
     return hour >= begin && hour < end;
@@ -22,55 +22,77 @@ function setMaskStyle(mask) {
     mask.style.zIndex = '99999999';
 }
 
-function closeTab(count = 10) {
-    // 倒计时关闭标签页
-    const interval = setInterval(() => {
-        count--;
-        if (count <= 0) {
-            clearInterval(interval);
-            window.close();
-        }
-    }, 1000);
-    return interval;
+class TabManager {
+    constructor() {
+        this.interval = null;
+    }
+    closeAfter(count, handle) {
+        this.count = count;
+
+        handle();
+        this.interval = setInterval(() => {
+            this.count--;
+            handle();
+            if (this.count <= 0) {
+                clearInterval(this.interval);
+                console.log(this.interval)
+                console.log('关闭页面');
+                window.close();
+            }
+        }, 1000);
+    }
+    cancelClose() {
+        clearInterval(this.interval);
+        this.interval = null;
+    }
 }
 
 (function () {
-    let interval;
     if (isWorkingHours()) {
-        interval = closeTab();
+        let tabManager = new TabManager();
 
-        const mask = document.createElement('div');
-        mask.innerHTML = '<p>WARNING: Working Time.<br /> Please do not use this website during working hours.</p><br /><br />';
-        setMaskStyle(mask);
+
+        const pageMask = document.createElement('div');
+        pageMask.innerHTML = '<p>WARNING: Working Time.<br /> Please do not use this website during working hours.</p><br /><br />';
+        setMaskStyle(pageMask);
+
+        const closeTips = document.createElement('p');
+        tabManager.closeAfter(3, () => {
+            closeTips.innerHTML = `This page will be closed in ${tabManager.count} seconds.`;
+        });
 
         // 关闭按钮
-        const close = document.createElement('a');
-        close.href = 'javascript:void(0)';
-        close.innerHTML = 'Close this page now';
-        close.addEventListener('click', () => {
+        const closeButton = document.createElement('a');
+        closeButton.href = 'javascript:void(0)';
+        closeButton.innerHTML = 'Close this page now';
+        closeButton.addEventListener('click', () => {
             window.close();
         });
 
         // 摸鱼按钮
-        const visit = document.createElement('a');
-        visit.href = 'javascript:void(0)';
-        visit.innerHTML = '摸会儿鱼';
-        visit.addEventListener('click', () => {
-            clearInterval(interval);
-            mask.remove();
+        const visitButton = document.createElement('a');
+        visitButton.href = 'javascript:void(0)';
+        visitButton.innerHTML = '摸会儿鱼';
+        visitButton.addEventListener('click', () => {
+            tabManager.cancelClose();
+            pageMask.remove();
         });
 
-        window.onkeydown = (e) => {
-            if (e.key === 'escape') {
-                clearInterval(interval);
-                mask.remove();
+        // ESC 开始摸鱼
+        window.onkeyup = (e) => {
+            if (e.keyCode === 27) {
+                tabManager.cancelClose();
+                pageMask.remove();
             }
         }
 
-        mask.appendChild(close);
-        mask.appendChild(visit);
+        pageMask.appendChild(closeTips);
+        pageMask.appendChild(closeButton);
+        pageMask.appendChild(visitButton);
 
         // 添加遮罩到页面
-        document.body.appendChild(mask);
+        document.body.appendChild(pageMask);
+    } else {
+        console.log('非工作时间，尽情摸鱼吧！');
     }
 })()
