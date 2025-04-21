2ad9b6fa70af3270b8436febd12f775dae90ba37
Z <wzhang@stu.xjtu.edu.cn>
2025-04-21 16:42:27 +0800

    update

--- scripts/baidu.js
+++ scripts/baidu.js
@@ -1,19 +1,22 @@
 const remove_search_box_placeholder = () => {
     // Remove the placeholder of the search input box
-    const searchInput = document.getElementById('kw');
+    const searchInput = document.getElementById("kw");
     if (searchInput) {
+        console.log(searchInput.placeholder);
+        searchInput.placeholder = "";
         const observer = new MutationObserver(() => {
-            if (searchInput.placeholder != '') {
-                searchInput.placeholder = '';
+            console.log(searchInput.placeholder);
+            if (searchInput.placeholder != "") {
+                searchInput.placeholder = "";
             }
         });
         observer.observe(searchInput, {
             attributes: true,
-            attributeFilter: ['placeholder']
+            attributeFilter: ["placeholder"],
         });
     }
-}
+};
 
-remove_search_box_placeholder();
-// window.addEventListener('load', function () {
-// });
+window.addEventListener("load", function () {
+    remove_search_box_placeholder();
+});
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,16 +1,16 @@
 // 避免该文件多次执行
 
-const _addEventListener = EventTarget.prototype.addEventListener;
-EventTarget.prototype.addEventListener = function (type, listener, options) {
-    if (!this.eventListeners) {
-        this.eventListeners = {};
-    }
-    if (!this.eventListeners[type]) {
-        this.eventListeners[type] = new Set();
-    }
-    this.eventListeners[type].add(listener);
-    _addEventListener.call(this, type, listener, options);
-}
+// const _addEventListener = EventTarget.prototype.addEventListener;
+// EventTarget.prototype.addEventListener = function (type, listener, options) {
+//     if (!this.eventListeners) {
+//         this.eventListeners = {};
+//     }
+//     if (!this.eventListeners[type]) {
+//         this.eventListeners[type] = new Set();
+//     }
+//     this.eventListeners[type].add(listener);
+//     _addEventListener.call(this, type, listener, options);
+// }
 
 async function getChromeStorage(key) {
     let keys = key;
