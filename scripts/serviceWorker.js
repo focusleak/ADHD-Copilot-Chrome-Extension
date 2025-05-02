56f922293e6c96474b3001d8f3aadabe05d9fb6b
Z <zw9409@126.com>
2025-05-02 11:23:10 +0800

    update

--- scripts/serviceWorker.js
+++ scripts/serviceWorker.js
@@ -16,18 +16,56 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
                 url: 'https://scholar.google.com/scholar?q=' + info.selectionText
             });
             break;
+        case 'searchByBaidu':
+            chrome.tabs.create({
+                url: 'https://www.baidu.com/s?wd=' + info.selectionText
+            });
+            break;
+        case 'searchByZhihu':
+            chrome.tabs.create({
+                url: 'https://www.zhihu.com/search?type=content&q=' + info.selectionText
+            });
+            break;
+        case 'searchBySogou':
+            chrome.tabs.create({
+                url: 'https://www.sogou.com/web?query=' + info.selectionText
+            });
+            break;
+        case 'searchByWexin':
+            chrome.tabs.create({
+                url: 'https://weixin.sogou.com/weixin?type=2&query=' + info.selectionText
+            });
+            break;
+        case 'searchByWeibo':
+            chrome.tabs.create({
+                url: 'https://s.weibo.com/weibo/' + info.selectionText
+            });
+            break;
+        case 'searchByXiaoHongShu':
+            chrome.tabs.create({
+                url: 'https://www.xiaohongshu.com/search_result?keyword=' + info.selectionText + '&source=web_explore_feed'
+            });
+            break;
+        case 'searchByBiliBili':
+            chrome.tabs.create({
+                url: 'https://search.bilibili.com/all?keyword=' + info.selectionText
+            });
+            break;
         case 'clearCookiesLocalStorage':
             chrome.scripting.executeScript({
                 target: { tabId: tab.id },
                 files: ["scripts/service/clearCookiesLocalStorage.js"]
             });
+            break;
         case 'readLater':
             chrome.scripting.executeScript({
                 target: { tabId: tab.id },
                 files: ["scripts/service/readLater.js"]
             });
+            break;
         default:
             console.log('Standard context menu item clicked.');
+            break;
     }
 });
 chrome.runtime.onInstalled.addListener(function () {
@@ -47,6 +85,41 @@ chrome.runtime.onInstalled.addListener(function () {
         id: 'searchByGoogle'
     });
     chrome.contextMenus.create({
+        title: '使用百度搜索',
+        contexts: ['selection'],
+        id: 'searchByBaidu'
+    });
+    chrome.contextMenus.create({
+        title: '使用知乎搜索',
+        contexts: ['selection'],
+        id: 'searchByZhihu'
+    });
+    chrome.contextMenus.create({
+        title: '使用搜狗搜索',
+        contexts: ['selection'],
+        id: 'searchBySogou'
+    });
+    chrome.contextMenus.create({
+        title: '使用微信搜索',
+        contexts: ['selection'],
+        id: 'searchByWexin'
+    });
+    chrome.contextMenus.create({
+        title: '使用微博搜索',
+        contexts: ['selection'],
+        id: 'searchByWeibo'
+    });
+    chrome.contextMenus.create({
+        title: '使用小红书搜索',
+        contexts: ['selection'],
+        id: 'searchByXiaoHongShu'
+    });
+    chrome.contextMenus.create({
+        title: '使用哔哩哔哩搜索',
+        contexts: ['selection'],
+        id: 'searchByBiliBili'
+    });
+    chrome.contextMenus.create({
         title: '使用谷歌学术搜索',
         contexts: ['selection'],
         id: 'searchByGoogleScholar'
@@ -55,5 +128,5 @@ chrome.runtime.onInstalled.addListener(function () {
         title: '稍后再看',
         contexts: ['page'],
         id: 'readLater'
-    })
+    });
 });
--- scripts/zhihu.js
+++ scripts/zhihu.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', function () {
     // Remove the placeholder of the search input box
-    const searchInputList = document.querySelectorAll('.SearchBar-input input');
-    searchInputList.forEach(searchInput => {
+    document.querySelectorAll('.SearchBar-input input').forEach(searchInput => {
         if (searchInput) {
             searchInput.placeholder = '';
             const observer = new MutationObserver(() => {
@@ -14,5 +13,38 @@ window.addEventListener('load', function () {
                 attributeFilter: ['placeholder']
             });
         }
-    })
+    });
+
+    // 链接直接跳转
+    document.querySelector('body').addEventListener('click', function (e) {
+        // a标签
+        if (e.target.tagName === 'A') {
+            const a = e.target;
+            let href = a.href;
+            if (href.includes('https://link.zhihu.com/?target')) {
+                e.preventDefault();
+                const url = new URL(href).searchParams.get('target');
+                window.open(url, '_blank');
+            }
+        }
+
+        // span标签
+        // e.g. https://zhuanlan.zhihu.com/p/30400486089
+        if (e.target.className === 'LinkCard-title two-line' && e.target.tagName === 'SPAN') {
+            const span = e.target;
+            const parent = span.parentElement;
+            const grandparent = parent.parentElement;
+            if (parent.className === 'LinkCard-contents' && grandparent.className === 'LinkCard new css-biylet') {
+                // 获取最近的祖先a节点
+                const url = span.closest('a').dataset.text;
+                // 阻止其他点击事件
+                e.stopPropagation();
+                window.open(url, '_blank');
+            }
+        }
+    });
+
+    // 解除复制限制
+
+
 });
